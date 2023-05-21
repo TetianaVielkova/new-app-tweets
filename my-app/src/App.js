36515c5575b49704c7ef23f63c2856b6f4609bb0
TanyaVielkova <106912349+TetianaVielkova@users.noreply.github.com>
2023-05-21 12:42:28 +0300

    Update App

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Tweets
+          Tweets new 
         </p>
         <a
           className="App-link"
